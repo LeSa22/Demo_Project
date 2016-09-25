3938b26370efe65a300ba613c9babc2d4e60187e
LeSa22 <tranlesa22@gmail.com>
2016-09-25 10:50:21 +0700

    Sign up

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+
+  # Returns the Gravatar for the given user.
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    image_tag('user.png', alt: user.name, class: "gravatar")
+  end
 end

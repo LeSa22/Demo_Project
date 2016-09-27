fec5220b3c7922bf84a61a048b90960a94a36e7e
LeSa22 <tranlesa22@gmail.com>
2016-09-27 15:12:27 +0700

    showing-delete-update users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
   # Returns the Gravatar for the given user.
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 50 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     image_tag('user.png', alt: user.name, class: "gravatar")
   end
--- db/migrate/20160927043939_add_picture_to_entries.rb
+++
@@ -1,5 +0,0 @@
-class AddPictureToEntries < ActiveRecord::Migration
-  def change
-    add_column :entries, :picture, :string
-  end
-end

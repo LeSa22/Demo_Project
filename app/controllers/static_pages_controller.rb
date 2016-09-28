19cb889ab295e25385368a277d8263ec88a970b8
LeSa22 <tranlesa22@gmail.com>
2016-09-28 15:39:07 +0700

    follow

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
   	if logged_in?
   	@entry = current_user.entries.build if logged_in?
-  	@feed_items = current_user.feed.paginate(page: params[:page])
+  	 @feed_items = current_user.feed.paginate(page: params[:page])
   	end
   end
 
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,7 +5,9 @@ module UsersHelper
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     if user.picture?
     	image_tag(user.picture.url , alt: user.name, class: "gravatar")
-  
+  	else
+  		image_tag("user.png" , alt: user.name, class: "gravatar")
     end
+
   end
 end
